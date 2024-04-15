2813818336963858a65c7bb6c7da65e28d9192bc
zarnigor <zarnigorasatova52@gmail.com>
2024-04-15 15:55:34 +0500

    feature

---
+++ app.js
@@ -0,0 +1,40 @@
+// 1 - task
+let bobName = "Bob";
+
+console.log(
+    `salom ${bobName}`
+)
+
+// 2 - task
+
+function calculateGrade(marks) {
+
+    const totalMarks = marks.length;
+
+
+    const totalSum = marks.reduce((sum, mark) => sum + mark, 0);
+
+
+    const average = totalSum / totalMarks;
+
+
+    if (average >= 90 && average <= 100) {
+        return 'A';
+    } else if (average >= 80 && average < 90) {
+        return 'B';
+    } else if (average >= 70 && average < 80) {
+        return 'C';
+    } else if (average >= 60 && average < 70) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+const marks = [80, 80, 50];
+console.log(calculateGrade(marks)); // Natija: 'C'
+
+// 3 - task
+
+
+// uchinchi task yoq
